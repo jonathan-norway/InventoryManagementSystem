f81ad44b01857e45c5ab7bfca356729b8bd82e5c
Chrissay97 <94568817+Chrissay97@users.noreply.github.com>
2021-12-03 09:16:07 -0600

    Update settings.py
    updated the database in settings,

--- djangoProject2/djangoProject2/settings.py
+++ djangoProject2/djangoProject2/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'djangoProject2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '',
+        'USER': 'root',
+        'PASWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
