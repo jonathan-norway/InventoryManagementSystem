2d54b32799f4f37529b4d2d9dd1e7d6db87aa699
Chrissay97 <94568817+Chrissay97@users.noreply.github.com>
2021-12-05 17:11:54 -0600

    Update project_product_info.sql

--- djangoProject2/project_product_info.sql
+++ djangoProject2/project_product_info.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
 --
--- Host:     Database: project
+-- Host: localhost     Database: project
 -- ------------------------------------------------------
 -- Server version	8.0.27
 
